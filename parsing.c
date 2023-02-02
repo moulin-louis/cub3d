af0192ec58c4b293295edadd44347dd8e1e24ca2
Mael PIGNET <you@example.com>
2023-02-02 13:38:05 +0100

    struct

--- parsing.c
+++ parsing.c
@@ -1,42 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cub3d.h                                            :+:      :+:    :+:   */
+/*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mpignet <mpignet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/02 13:04:16 by loumouli          #+#    #+#             */
-/*   Updated: 2023/02/02 13:37:41 by mpignet          ###   ########.fr       */
+/*   Created: 2023/02/02 13:34:07 by mpignet           #+#    #+#             */
+/*   Updated: 2023/02/02 13:34:23 by mpignet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CUB3D_H
-#define CUB3D_H
-
-# include <unistd.h>
-# include <stdlib.h>
-# include <stdio.h>
-# include <string.h>
-# include <fcntl.h>
-# include <errno.h>
-# include <math.h>
-
-typedef struct s_map
-{
-	void	*mlx;
-	void	*window;
-
-	unsigned char	floor[3];
-	unsigned char	ceiling[3];
-
-	void	*nord;
-	void	*south;
-	void	*east;
-	void	*west;
-
-	char	**map;
-	int		win_width;
-	int		win_height;
-}	t_map;
-
-#endif
+#include <cub3d.h>
