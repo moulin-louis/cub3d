efb74b3922b259aa83898f238ae4e160106fcad3
Louis Moulin <loumouli@e3r2p23.clusters.42paris.fr>
2023-02-13 15:41:15 +0100

    rotate minimap

--- bonus/src/rendering/minimap_bonus.c
+++ bonus/src/rendering/minimap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: loumouli <loumouli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/10 10:53:07 by loumouli          #+#    #+#             */
-/*   Updated: 2023/02/13 15:34:13 by loumouli         ###   ########.fr       */
+/*   Updated: 2023/02/13 15:40:37 by loumouli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,15 +43,16 @@ void	display_pixel(t_data *data, int x, int y, int offset)
 	img_data.raw_data = mlx_get_data_addr(img_data.img, &img_data.bpp,
 			&img_data.size_line, &img_data.endian);
 	color = 0;
-	if (x == (int)data->pos_x && y == (int)data->pos_y)
+	if (y == (int)data->pos_x && x == (int)data->pos_y)
 		color = get_rgb(0, 0, 255);
-	else if (data->map[x][y] == 1)
-		color = get_rgb(255, 0, 0);
-	else if (data->map[x][y] == 0)
-		color = get_rgb(0, 255, 0);
-	img_pix_put(&img_data, (x * 2) + offset, y * 2, color);
+	else if (data->map[y][x] == 1)
+		color = get_rgb(128, 56, 0);
+	else if (data->map[y][x] == 0)
+		color = get_rgb(128, 128, 128);
+	img_pix_put(&img_data, (x * 2) + offset, (y * 2) + 0, color);
 	img_pix_put(&img_data, (x * 2) + offset, (y * 2) + 1, color);
 	img_pix_put(&img_data, (x * 2) + offset, (y * 2) + 2, color);
+	img_pix_put(&img_data, (x * 2) + offset, (y * 2) + 3, color);
 
 }
 
@@ -62,17 +63,18 @@ void	draw_minimap(t_data *data)
 	int			y;
 
 	
-	x = -1;
 	if (data->len_map_x > HEIGHT || data->len_map_y > WIDTH)
 		return ;
-	while (++x < data->len_map_x )
+	y = -1;
+	while (data->map[++y])
 	{
-		y = -1;
-		while (++y < data->len_map_y)
+		x = -1;
+		while (data->map[y][++x] != END)
 		{
-			display_pixel(data, x, y, 0);
-			display_pixel(data, x, y, 1);
-			display_pixel(data, x, y, 2);
+			display_pixel(data, x , y, 0);
+			display_pixel(data, x , y, 1);
+			display_pixel(data, x , y, 2);
+			display_pixel(data, x , y, 3);
 		}
 	}
 }
